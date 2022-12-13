864336a2237289012f75db06cc7cdd115fcb08d7
Sumarajanahally <sumarajanahally@gmail.com>
2022-12-12 21:36:01 -0800

    Added Domain

--- EventCatalogAPI/Program.cs
+++ EventCatalogAPI/Program.cs
@@ -1,13 +1,23 @@
-var builder = WebApplication.CreateBuilder(args);
+using EventCatalogAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<EventCatalogContext>(options => options.UseSqlServer(configuration["ConnectionString"]));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProviders = scope.ServiceProvider;
+    var context = serviceProviders.GetRequiredService<EventCatalogContext>();
+    
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
