564a4860f663e549ee7315e75bce91e557b066b1
Swathi Choppavarapu <v-swchop@microsoft.com>
2022-12-16 20:45:43 -0800

    Final code changes
    Final code changes

--- EventCatalogAPI/Controllers/EventPicController.cs
+++ EventCatalogAPI/Controllers/EventPicController.cs
@@ -16,7 +16,7 @@ namespace EventCatalogAPI.Controllers
         public IActionResult GetImage(int id)
         {
             var webRoot = _env.WebRootPath;
-            var path = Path.Combine($"{webRoot}/Pics/", $"Image{id}.jpg");
+            var path = Path.Combine($"{webRoot}/Pics/", $"{id}.jpg");
             var buffer = System.IO.File.ReadAllBytes(path);
             return File(buffer, "image/jpeg");
         }
