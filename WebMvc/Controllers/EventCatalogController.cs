904b87cdafe449a6c2c82076b8e66409531ac423
Sumarajanahally <sumarajanahally@gmail.com>
2023-01-10 18:33:11 -0800

    Added Controller and Services

---
+++ WebMvc/Controllers/EventCatalogController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebMvc.Services;
+
+namespace WebMvc.Controllers
+{
+    public class EventCatalogController : Controller
+    {
+        private readonly IEventCatalogService _service;
+        public EventCatalogController(IEventCatalogService service)
+        {
+            _service = service;
+        }
+    
+        public async Task<IActionResult> Index( int? page)
+        {
+            var itemsOnPage = 6;
+            var events = await _service.GetEventsAsync(page ?? 0, itemsOnPage);
+        
+            return View();
+        }
+    }
+}
---
+++ WebMvc/Services/IEventCatalogService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using WebMvc.Models;
+
+namespace WebMvc.Services
+{
+    public interface IEventCatalogService
+    {
+        Task<EventCatalog> GetEventsAsync(int page, int size);
+        Task<IEnumerable<SelectListItem>> GetEventOrganizersAsync();
+
+        Task<IEnumerable<SelectListItem>> GetEventCategoriesAsync();
+
+    }
+}
