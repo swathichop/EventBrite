60465986b8812970c513fe2be76842ee85e56030
sravanthiramayanam <114696618+sravanthiramayanam@users.noreply.github.com>
2023-01-18 18:30:09 -0800

    finished creating cart microservice
    need to work on webclient

---
+++ CartApi/Program.cs
@@ -0,0 +1,61 @@
+using CartApi.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using StackExchange.Redis;
+using System.IdentityModel.Tokens.Jwt;
+
+var builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
+// Add services to the container.
+builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddTransient<ICartRepository, RedisCartRepository>();
+builder.Services.AddSingleton<ConnectionMultiplexer>(cm =>
+{
+    var config = ConfigurationOptions.Parse(
+            configuration["ConnectionString"], true);
+    config.ResolveDns = true;
+    config.AbortOnConnectFail = true;
+    return ConnectionMultiplexer.Connect(config);
+});
+// prevent from mapping "sub" claim to nameidentifier.
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+
+var identityUrl = configuration["IdentityUrl"];
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.Authority = identityUrl.ToString();
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = false,
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+    options.Audience = "basket";
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
